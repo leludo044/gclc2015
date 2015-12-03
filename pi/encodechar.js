16688a44a61e32fc12b2ce894f2052d90e3ba52d
codepostal <romain.coiffard@laposte.fr>
2015-12-03 23:17:00 +0100

    optim encodechar

--- pi/encodechar.js
+++ pi/encodechar.js
@@ -1,23 +1,28 @@
 module.exports = (function() {
-var encodechar = function(text){
-    var res='';
-    for (var index = 0; index < text.length; index++) {
-        var code = text[index].charCodeAt(0);
-        if(code<=32){
-			var x = (code).toString(8);
-			//console.log(code,x)
-			if(x<10){
-				res += '#00'+x;
-			}else if(x<100){
-				res += '#0'+x;
-			}else{
-    	        res += '#'+x;
-			}
-        }else{
-            res += text[index]
+    var DBLZERO = '#00';
+    var SIMPLEZERO = '#0';
+    var SHARP = '#';
+    
+    var encodechar = function(text){
+        var res='';
+        for (var index = 0; index < text.length; index++) {
+            var c = text[index];
+            var code = c.charCodeAt(0);
+            if(code<=32){
+                var x = (code).toString(8);
+                //console.log(code,x)
+                if(x<10){
+                    res += DBLZERO+x;
+                }else if(x<100){
+                    res += SIMPLEZERO+x;
+                }else{
+                    res += SHARP+x;
+                }
+            }else{
+                res += c;
+            }
         }
+        return res;
     }
-    return res;
-}
-return encodechar;
+    return encodechar;
 }());
