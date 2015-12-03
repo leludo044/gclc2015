d6e16af9f96dbf07cac634fe97cc1194f73f245c
divag <cieplicki@gmail.com>
2015-12-03 09:52:41 +0100

    Optimisation des transferts : le timestamp n'est véhiculé que partiellement pour diminuer les trames.

--- pi.js
+++ pi.js
@@ -11,18 +11,23 @@ var send = function (message) {
     var sender = dgram.createSocket('udp4');
     sender.send(message, 0, message.length, PORT_SERVER, HOST_SERVER, function (err, bytes) {
         if (err) throw err;
-        console.log('UDP message sent to ' + HOST_SERVER + ':' + PORT_SERVER);
+        //console.log('UDP message sent to ' + HOST_SERVER + ':' + PORT_SERVER);
         sender.close();
     });
 };
 
+//Mémorisation d'un timestamp de référence, pour n'envoyer que le delta lors de la transmission des messages :
+var initialTime = Math.floor(new Date() / 1000);
+send(new Buffer(initialTime.toString()));
+console.log('Service launched - initial timestamp : ' + initialTime);
+
 
 var client = dgram.createSocket('udp4');
 client.on('message', function (msg, rinfo) {
-    console.log('Received %d bytes from %s:%d\n',
-        msg.length, rinfo.address, rinfo.port, msg.toString());
+    //console.log('Received %d bytes from %s:%d\n', msg.length, rinfo.address, rinfo.port, msg.toString());
     
-    var timestamped = Math.floor(new Date() / 1000) + ':'+msg.toString() ;
+    var messageTime = Math.floor(new Date() / 1000) - initialTime;
+    var timestamped = messageTime + ':'+msg.toString() ;
     send(new Buffer(timestamped));
 });
 client.bind(PORT);
--- vm.js
+++ vm.js
@@ -6,23 +6,38 @@ var os = require('os');
 var fs = require('fs');
 
 var fileName = "/opt/gclc/gclc.log";
-  
+
+var initialTimestamp = null;  
+var initialDate = "";
 
 var client = dgram.createSocket('udp4');
 client.on('message', function(msg, rinfo) {
-  console.log('Received %d bytes from %s:%d\n',
-              msg.length, rinfo.address, rinfo.port, msg.toString());
-              convertDate(msg.toString(),writeMessage);
+  //console.log('Received %d bytes from %s:%d\n', msg.length, rinfo.address, rinfo.port, msg.toString());
+  
+  convertDate(msg.toString(),writeMessage);
   
 });
 client.bind(PORT) ;
 
+
 function convertDate(msg, callback){
-  var tab = msg.split(':');
-  //console.log(tab)
-  var message = formatDay()+" "+new Date(parseInt(tab[0])*1000).toLocaleTimeString()+' '+tab[1];
-  //console.log(message);
-  callback(message);
+
+  if (msg.indexOf(':') == -1)
+  {
+    initialTimestamp = parseInt(msg) * 1000;
+    // console.log('initialTimestamp',initialTimestamp);
+    initialDate = formatDay(new Date(initialTimestamp));
+
+    convertDate = function(msg, callback) {
+      var index = msg.indexOf(':');
+      var msgTimeStamp = parseInt(msg.substring(0,index))*1000;
+      // console.log('msgTimeStamp', msgTimeStamp);
+      var bodyMsg = msg.substring(index+1,msg.length);
+      var message = initialDate + new Date(initialTimestamp + msgTimeStamp).toLocaleTimeString()+' '+bodyMsg;
+      //console.log(message);
+      callback(message);
+    }
+  }
 }
 
 function writeMessage(msg){
@@ -31,22 +46,21 @@ function writeMessage(msg){
           return console.log(err);
       }
   
-      console.log("The file was saved!",fileName);
+      // console.log("The file was saved!",fileName);
   }); 
   
 }
 
-var formatDay = function () {
+var formatDay = function (date) {
 
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var d = new Date();
-    var day = d.getDate();
+    var day = date.getDate();
 
-    var formatedDay = months[d.getMonth()] + " ";
+    var formatedDay = months[date.getMonth()] + " ";
     if (day < 10) {
-        formatedDay += " " + d.getDate();
+        formatedDay += " " + date.getDate();
     } else {
-        formatedDay += d.getDate();
+        formatedDay += date.getDate();
     }
-    return formatedDay ;
+    return formatedDay + " ";
 };
