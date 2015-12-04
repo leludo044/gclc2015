61253d1a90fc29313fb7e886f18b2a843ca9eff1
codepostal <romain.coiffard@laposte.fr>
2015-12-04 13:31:48 +0100

    suppression console.log dans compress/uncompress

--- pi/compress.js
+++ pi/compress.js
@@ -2,7 +2,7 @@ module.exports = (function() {
     var zlib = require('zlib');
     var compress = function (data, callback){
         return zlib.gzip(data,function(err, data){
-            console.log("gzip.return",data) 
+            // console.log("gzip.return",data) 
             if(err)throw err;
             callback(err, data);
         });
